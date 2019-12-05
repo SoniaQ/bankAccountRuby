c5d378596f3d197c95fb16ea31b251c3ca926c11
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-05 14:14:03 +0000

    Create failing unit test for second user story.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,6 +2,11 @@ require 'bankaccount'
 
 describe BankAccount do
 
+  it 'can be opened with a desired intitial balance' do
+    bank = BankAccount.new(10)
+    expect(bank.balance).to eq 10
+  end
+
   it 'can deposit money in the bank account' do
     bank = BankAccount.new
     expect{ bank.deposit(5) }.to change{ bank.balance }.by 5
