0b3d333c164de8543add86e76adbdaa111b6613e
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-05 14:11:19 +0000

    Refactor unit test for first user story.

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,8 +4,7 @@ describe BankAccount do
 
   it 'can deposit money in the bank account' do
     bank = BankAccount.new
-    bank.deposit(5)
-    expect(bank.balance).to eq 5
+    expect{ bank.deposit(5) }.to change{ bank.balance }.by 5
   end
 
 end
